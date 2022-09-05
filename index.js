2e4b2cde4358e85b6db919741b2c1aceaa08850d
Code <kienlc72@wru.vn>
2022-09-05 17:04:59 +0700

    up

---
+++ index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if($(this).scrollTop()){
+            $('nav').addClass('sticky');
+        }
+        else{
+            $('nav').removeClass('sticky');
+        }
+    })
+})
